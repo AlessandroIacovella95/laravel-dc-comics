644af8365f00fab80c3107b02450ab0ca9d3eb07
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-10-18 18:27:53 +0200

    add route and db

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\ComicController;
 
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PageController::class, 'index'])->name('home');
+
+Route::get('/comics', function(){
+    $comics= config('db.comic');
+    return view('comics', compact('comics'));
+})->name('index');
