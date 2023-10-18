8c5463d6c70b7ad3d8d5c76a66cb31d7d100e8ad
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-10-18 20:38:47 +0200

    add file index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,21 +1,38 @@
 @extends('layouts.app')
 
+@section('css')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+@endsection
+
 @section('main-content')
   <section class="container mt-5">
-
-    @forelse($comics as $comic)
-      <p>
-        <strong>Titolo</strong>: {{ $comic->title }} <br>
-        <strong>Descrizione</strong>: {{ $comic->description }} <br>
-        <strong>Immagine</strong>: {{ $comic->thumb }}
-        <strong>Prezzo</strong>: {{ $comic->price }}
-        <strong>Serie</strong>: {{ $comic->series }}
-        <strong>Data di uscita</strong>: {{ $comic->sale_date }}
-        <strong>Genere</strong>: {{ $comic->type }}
-      </p>
-      <hr>
-    @empty
-      <h2>Non ci sono risultati</h2>
-    @endforelse
+      <div class="container">  
+        <a href="{{route('comics.create')}}" class="btn btn-success">
+            Crea nuovo fumetto
+        </a>
+        <h1 class="mb-3">Comics</h1>
+        <div class="row">
+          @foreach ($comics as $comic)
+            <div class="col-lg-4 col-md-6 mb-4">
+              <div class="card h-100">
+                <div class="card-body">
+                  <p class="card-text">
+                    <strong>Titolo:</strong> {{ $comic->title }}<br>
+                    <strong>Descrizione:</strong> {{ $comic->description }}<br>
+                    <img src="{{ $comic->thumb }}" alt=""><br>
+                    <strong>Prezzo:</strong> {{ $comic->price }} <br>
+                    <strong>Serie:</strong> {{ $comic->series }} <br>
+                    <strong>Data di uscita:</strong> {{ $comic->sale_date }} <br>
+                    <strong>Genere:</strong> {{ $comic->type }} <br>
+                    <a href="{{ route('comics.show', $comic)}}">
+                        <i class="custom fa-solid fa-eye"></i>
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          @endforeach
+        </div>
+      </div>
   </section>
 @endsection
