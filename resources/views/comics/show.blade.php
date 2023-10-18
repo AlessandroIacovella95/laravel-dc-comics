90d0dbde94e4fc6f5db956e2e906f03aac5a000c
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-10-18 20:38:53 +0200

    add file show

---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('main-content')
+<div class="container mt-5">
+    <h1>{{ $comic->title }}</h1>
+    <div class="row g-4">
+        
+        <div class="col-3">
+            <img src="{{ $comic->thumb }}" alt="">
+        </div>
+        
+        <div class="col-3">
+            <strong>Prezzo:</strong> {{ $comic->price }}
+        </div>
+        
+        <div class="col-3">
+            <strong>Serie:</strong> {{ $comic->series }}
+        </div>
+        
+        <div class="col-3">
+            <strong>Data di uscita::</strong> {{ $comic->sale_date }}
+        </div>
+        
+        <div class="col-3">
+            <strong>Genere:</strong> {{ $comic->type }}
+        </div>
+
+        <div class="col-12">
+            <strong>Descrizione:</strong> {{ $comic->description }}
+        </div>
+    </div>
+</div>
+@endsection
